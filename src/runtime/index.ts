5c5f65ae8ee936c6cb71e8cf27430269848da2f4
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-03-16 14:52:55 +0100

    feat: add useVIntl composable as an alternative to useI18n

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,3 +1,3 @@
-export { useI18n } from './useI18n.js'
+export { useI18n, useVIntl } from './useI18n.js'
 export { useFormatters } from './useFormatters.js'
 export { useTranslate } from './useTranslate.js'
