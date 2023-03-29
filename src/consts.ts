5b43aeb6e55b41369b7a3bc622cbcb0b1479f46f
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-03-29 16:20:04 +0200

    fix(composables): use string as injection key
    Because reasons

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,1 @@
-import type { InjectionKey } from 'vue'
-import type { IntlController } from './controller.js'
-
-export const controllerKey = Symbol('vueIntlController') as InjectionKey<
-  IntlController<any>
->
+export const controllerKey = '__vintl_controller'
