653c968a3e5407004db9e0d8dbed9b3fe1133ed4
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-06-20 16:00:37 +0200

    Add missing FormattedNumber types

--- src/components/number.ts
+++ src/components/number.ts
@@ -1,6 +1,10 @@
 import {
   defineSimpleFormatterComponent,
   definePartsFormatterComponent,
+  type SimpleFormatterComponentSlots,
+  type SimpleFormatterComponentProps,
+  type PartsFormatterComponentProps,
+  type PartsFormatterComponentSlots,
 } from './utils/index.ts'
 
 // This is required so that TypeScript cannot infer the types.
@@ -8,6 +12,16 @@ import type {} from '@formatjs/ecma402-abstract'
 
 export const FormattedNumber = defineSimpleFormatterComponent('formatNumber')
 
+export type FormattedNumberProps = SimpleFormatterComponentProps<'formatNumber'>
+
+export type FormattedNumberSlots = SimpleFormatterComponentSlots<'formatNumber'>
+
 export const FormattedNumberParts = definePartsFormatterComponent(
   'formatNumberToParts',
 )
+
+export type FormattedNumberPartsProps =
+  PartsFormatterComponentProps<'formatNumberToParts'>
+
+export type FormattedNumberPartsSlots =
+  PartsFormatterComponentSlots<'formatNumberToParts'>
