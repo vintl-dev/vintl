d338ea485a67f8867ffacf7a7d1240eb9e6edde8
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-06-20 20:27:00 +0200

    Make Fragment component accept VNodeChild

--- src/components/fragment.ts
+++ src/components/fragment.ts
@@ -1,9 +1,13 @@
-import { defineComponent, type VNode } from 'vue'
+import { defineComponent, type VNodeChild } from 'vue'
 
 interface FragmentProps {
-  of: VNode[] | VNode
+  of: VNodeChild
 }
 
 export const Fragment = defineComponent(
   (props: FragmentProps) => () => props.of,
+  {
+    // eslint-disable-next-line vue/require-prop-types
+    props: ['of'],
+  },
 )
